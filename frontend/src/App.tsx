4172fc5cb0169028529498fb17b5e99eb55f7f0a
Moaadb <lemangeurdepitta@gmail.com>
2026-01-28 15:43:19 +0100

    removed everything

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,9 @@
 import "./App.css"
-import { Routes, Route } from "react-router-dom"
-import Base from "./pages/Base"
-import Home from "./pages/Home"
-import Networth from "./pages/Networth"
-import Passwords from "./pages/Passwords"
-import Projects from "./pages/Projects"
-import Project from "./pages/Project"
-import NotFound  from "./pages/NotFound"
 
 function App() {
-    return (
-        <Routes>
-			<Route path="/" element={<Base />}> 
-				<Route path="" element={<Home />} /> 
-                <Route path="projects" element={<Projects />} /> 
-                <Route path="projects/:id" element={<Project/>} />
-                <Route path="networth" element={<Networth />} /> 
-                <Route path="passwords" element={<Passwords/>} /> 
-                <Route path="*" element={<NotFound/>} />
-			</Route>
-        </Routes>
-    );
+    return ( 
+        <p>salut</p>
+    )
 }
 
 export default App;
--- frontend/src/pages/Networth.tsx
+++
@@ -1,3 +0,0 @@
-export default function Networth(){
-    return <p>Networth</p>
-} 
